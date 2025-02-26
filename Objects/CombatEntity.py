52a77fcbcfa3aab0566797b1c1f08cd3e3c5f745
uri <urino1234567@gmail.com>
2025-02-26 11:10:11 +0200

    Fixed bugs

--- Objects/CombatEntity.py
+++ Objects/CombatEntity.py
@@ -3,11 +3,11 @@ class CombatEntity:
 
     def __init__(self, force_side):
         self.force_side = force_side
-        self.id = entity_counter
-        entity_counter += 1
+        self.id = CombatEntity.entity_counter
+        CombatEntity.entity_counter += 1
 
     def get_id(self):
         return self.id
 
-    def get_current_state():
+    def get_current_state(self):
         pass
