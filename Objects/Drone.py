6d0c034bac6d28f170c6ec0382b5fea7f4d4f9e2
ido levy nahum <idodo0070@gmail.com>
2025-02-26 09:35:13 +0200

    Added __str__ to the Drone class

--- Objects/Drone.py
+++ Objects/Drone.py
@@ -1,5 +1,6 @@
-from Entity import CombatEntity 
-from Enums.ForceSide import FroceSide
+from .CombatEntity import CombatEntity
+from .Enums.ForceSide import FroceSide
+
 class Drone(CombatEntity):
     def __init__(self, drone_type, current_location, current_speed, current_battery_level):
         self.drone_type = drone_type
@@ -8,8 +9,7 @@ class Drone(CombatEntity):
         self.current_battery_level = current_battery_level
 
     def __str__(self) -> str:
-        show_str = "RED" if self.drone_type == FroceSide.RED else "BLUE"
-        show_str += f" {str(self.current_location)} {self.current_speed}"
+        return f"{self.drone_type.name} location: {str(self.current_location)} speed: {self.current_speed}"
 
     def update_state():
         pass
