b51c54743db913c8a82f21cae7f5b8a278642e96
uri <urino1234567@gmail.com>
2025-02-26 09:19:04 +0200

    added Drone and CombatEntity

---
+++ Objects/CombatEntity.py
@@ -0,0 +1,4 @@
+class CombatEntity:
+    def __init__(self, force_side, id):
+        self.force_side = force_side
+        self.id = id
---
+++ Objects/Enums/ForceSide.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class FroceSide(Enum):
+    BLUE = 1
+    RED = 2
