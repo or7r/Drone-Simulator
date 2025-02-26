0194b81a1bd6e677cc27f1f581ca81427b5afedd
uri <urino1234567@gmail.com>
2025-02-26 11:25:48 +0200

    Added skeleton to drone

--- Objects/Drone.py
+++ Objects/Drone.py
@@ -1,11 +1,22 @@
 from Entity import CombatEntity 
+import Flight
 
 class Drone(CombatEntity):
-    def __init__(self, drone_type, current_location, current_speed, current_battery_level):
+    def __init__(self, drone_type, current_battery_level, force_side):
+        super(force_side)
+
         self.drone_type = drone_type
-        self.current_location = current_location
-        self.current_speed = current_speed
         self.current_battery_level = current_battery_level
+        self.is_flying = False
+
+    def get_current_location():
+        pass
+
+    def get_current_speed():
+        pass
 
     def update_state():
+        pass
+
+    def exec_flight():
         pass
